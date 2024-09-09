a7ce7b6263a8b9e285ef6f574388829777f8a782
Jaya Christina <jayachristina@gmail.com>
2024-09-09 16:35:44 +0530

    typos, remove debezium

--- contrib/postgresql-start/start-postgresql.sh
+++ contrib/postgresql-start/start-postgresql.sh
@@ -14,14 +14,9 @@ else
   psql -d $POSTGRESQL_DATABASE -w -c "grant all privileges on database ${POSTGRESQL_DATABASE} to ${POSTGRESQL_USER};"
 
   envsubst < $SQL_DIR/create.sql > /tmp/create.sql
-  envsubst < $SQL_DIR/setup-debezium.sql > /tmp/setup-debezium.sql
-
+  
   psql -d $POSTGRESQL_DATABASE -w < /tmp/create.sql
   psql -d $POSTGRESQL_DATABASE -w < $SQL_DIR/initialize.sql
 
-  psql -w -c "CREATE ROLE debezium REPLICATION LOGIN;"
-  psql --set=password="$POSTGRESQL_DEBEZIUM_PASSWORD" -w <<< "ALTER USER \"debezium\" WITH ENCRYPTED PASSWORD :'password';"
-  psql -d $POSTGRESQL_DATABASE -w -c "grant CREATE on database ${POSTGRESQL_DATABASE} to debezium;"
-
-  psql -d $POSTGRESQL_DATABASE -w < /tmp/setup-debezium.sql
+  
 fi
